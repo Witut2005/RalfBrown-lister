2a6d81259e047190e4e06c9d8e6553522e03ab3f
Witut2005 <wiktor.glogowski@onet.pl>
2022-07-30 10:55:38 +0200

    jupi

--- ralf.py
+++ ralf.py
@@ -1,29 +1,21 @@
 
 import argparse
 import os
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common import service
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 Parser = argparse.ArgumentParser()
-Parser.add_argument('-ax', help='value of ah register', required=False)
-Parser.add_argument('-ah', help='value of ah register', required=False)
-Parser.add_argument('-int', help='interrupt number', required=True)
+
+Parser.add_argument('-param', help='parameters', required=True)
 
 Args = Parser.parse_args()
 
-print(Args.int)
-print(Args.ah)
-print(Args.ax)
-
-try:
-    int(Args.int, 16)
-    int(Args.ah, 16)
-    int(Args.ax, 16)
-
-except TypeError:
-    print('1')
+int_number = int(Args.param[0] + Args.param[1], 16)
+Args.param = 'Int ' + Args.param
+print(Args.param)
 
 DRIVER_PATH = str(os.environ["HOME"]) + "/chromedriver/chromedriver"
 
@@ -36,9 +28,24 @@ driver.get('https://www.ctyme.com/intr/int.htm')
 
 interrupt_list = driver.find_elements(By.XPATH, '/html/body/center[2]/p[2]/table/tbody/tr/td/a')
 
-# for ints in interrupt_list:
-print(interrupt_list[Args.int].get_property('href'))
-interrupt_list[Args.int].click()
+interrupt_list[int_number].click()
+
+results = (driver.find_elements(By.XPATH, '/html/body/p[4]/a'), driver.find_elements(By.XPATH, '/html/body/p[4]/a/b'))
+
+print(results[1][0].text)
+
+x = 0
+while results[1][x].text != Args.param:
+    x += 1
+
+print(results[1][x].text)
+
+results[0][x].click()
+
+results = driver.find_elements(By.XPATH, '/html/body/pre')
+
+for result in results:
+    print(result.text)
 
 
 
